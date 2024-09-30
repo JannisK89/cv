caf80d30eb25c4eb1a1bf185ff628cb1e52dee79
JannisK89 <jannis.karanikis@gmail.com>
2024-09-30 19:16:37 +0200

    Fix grid issue

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>Jannis Karanikis</title>
+	<meta
+		name="description"
+		content="CV/Resume Page For Jannis Karanikis Software Engineer, Frontend Developer, Test Specialist and Cyber Security Enthusiast."
+	/>
+</svelte:head>;
 <div class="flex justify-center shadow">
 	<div class="lg:my-8 lg:grid grid-cols-8 lg:h-screen lg:w-3/4">
 		<slot />
